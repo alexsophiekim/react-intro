001f17f677821f63d527b33f9c88c4a412da926a
Sophie Kim <270010565@bun2-207-03.acg.ac.nz>
2019-10-22 14:54:27 +1300

    button working

--- src/Test.js
+++ src/Test.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 import './test.css';
 
 class Test extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colour:'red'
+    }
+    // this.clicked = this.clicked.bind(this);
+  }
+
+  // clicked(){
+  //     console.log('We clicked on a box');
+  //     console.log(this.state.colour);
+  // }
+  clicked = () => {
+    console.log('We clicked on a box');
+    console.log(this.state.colour);
+
+    if (this.state.colour == 'red') {
+      this.setState({
+        colour:'blue'
+      });
+    } else {
+      this.setState({
+        colour:'red'
+      });
+    }
+  }
+
   render(){
     return(
-      <div>
+      <div className={`link ${this.state.colour}`} onClick={this.clicked}>
+        {this.state.colour}
         <a href={this.props.href}>{this.props.message}</a>
       </div>
     )
