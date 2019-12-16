c40ec06289b657cc6e69d7c8aa3c274ab7f5d848
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-12-17 10:22:59 +1300

    button.js

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,9 +2,17 @@ import React, {Component} from 'react';
 import './Button.css';
 
 class Button extends Component{
+
+  clickButton = () => {
+    //console.log('button has been clicked');
+    if (this.props.btnClicked) {
+      this.props.btnClicked();
+    }
+  }
+
   render(){
     return(
-      <button className={this.props.btnType}>
+      <button className={this.props.btnType} onClick={this.clickButton}>
       {this.props.btnText}
       </button>
     )
